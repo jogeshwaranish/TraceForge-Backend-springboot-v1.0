ce358aa73684b2499be5cbda163aa20ef8a9e5cc
Anish Bellamkonda <ani.bellamkonda@gmail.com>
2025-11-04 08:44:28 -0600

    Need to address issue with path of git, if we fix it , then I managed to add the path from frotnend to backend selection and returning the generated markdown also.

--- src/main/java/com/TraceForge/AI/controller.java
+++ src/main/java/com/TraceForge/AI/controller.java
@@ -3,10 +3,7 @@ package com.TraceForge.AI;
 
 import com.TraceForge.AI.Service.GitService;
 import org.eclipse.jgit.api.errors.GitAPIException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
 import java.io.IOException;
@@ -14,9 +11,20 @@ import java.io.IOException;
 @RestController
 public class controller {
 
-    @GetMapping("/getMarkDown/")
-    public String getMarkDown() throws GitAPIException, IOException {
-        GitService gitService = new GitService(new File("/home/anishbellamkonda/Downloads/Expense_tracker/.git"));
-        return gitService.printCommits();
+    @PostMapping("/getMarkDown/")
+    public String getMarkDown(@RequestBody String body) throws GitAPIException, IOException {
+        System.out.println(body);
+        try {
+            File p1 = new File(body);
+            System.out.println("body exists and works with file");
+            GitService gitService = new GitService(new File(body));
+            return gitService.printCommits();
+        } catch(Exception e){
+            System.out.println(e.getMessage());
+            System.out.println("body not working");
+        }
+//      GitService gitService = new GitService(new File(path));
+//      return gitService.printCommits();
+        return "";
     }
 }
