7fd7de97a10c44594a869bfd56efe78387284379
Anish Bellamkonda <ani.bellamkonda@gmail.com>
2025-11-05 09:14:26 -0600

    Addressed file path issues when sent through request, now we fixed it and documentation works, great progress

--- src/main/java/com/TraceForge/AI/controller.java
+++ src/main/java/com/TraceForge/AI/controller.java
@@ -2,22 +2,26 @@ package com.TraceForge.AI;
 
 
 import com.TraceForge.AI.Service.GitService;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.google.api.client.json.Json;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Type;
 
 @RestController
 public class controller {
 
     @PostMapping("/getMarkDown/")
-    public String getMarkDown(@RequestBody String body) throws GitAPIException, IOException {
+    public String getMarkDown(@RequestBody JsonNode body) throws GitAPIException, IOException {
         System.out.println(body);
         try {
-            File p1 = new File(body);
+            String path = body.get("path").asText();
+            System.out.println(path);
             System.out.println("body exists and works with file");
-            GitService gitService = new GitService(new File(body));
+            GitService gitService = new GitService(new File(path));
             return gitService.printCommits();
         } catch(Exception e){
             System.out.println(e.getMessage());
